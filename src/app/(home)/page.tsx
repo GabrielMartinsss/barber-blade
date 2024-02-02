a86ffd482e232c737f4af66fef377c9414ab8843
Gabriel Martins <gabrielsilva3553@gmail.com>
2024-02-02 10:27:42 -0300

    feat: add initial barbershop-item component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,13 +3,18 @@ import { Header } from '@/components/header'
 import { ptBR } from 'date-fns/locale'
 import { InputSearch } from './components/input-search'
 import { BookingItem } from '@/components/booking-item'
+import { BarbershopItem } from './components/barbershop-item'
+import { databasePrisma } from '@/lib/prisma'
+import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area'
 
-export default function Home() {
+export default async function Home() {
+  const barbershops = await databasePrisma.barbershop.findMany()
+
   return (
-    <div className="space-y-6 px-5">
-      <Header />
+    <div className="space-y-6">
+      <Header className="px-5" />
 
-      <div>
+      <div className="px-5">
         <h2 className="text-xl dark:text-zinc-50">
           Olá, <span className="font-bold"> Faça seu login!</span>
         </h2>
@@ -20,13 +25,29 @@ export default function Home() {
         </span>
       </div>
 
-      <InputSearch />
+      <InputSearch className="px-5" />
 
-      <div className="space-y-3">
+      <div className="space-y-3 px-5">
         <p className="text-xs font-bold uppercase dark:text-zinc-500">
           Agendamentos
         </p>
         <BookingItem />
+      </div>
+
+      <div className="space-y-3">
+        <p className="px-5 text-xs font-bold uppercase dark:text-zinc-500">
+          Recomendados
+        </p>
+
+        <ScrollArea className="w-full pl-5">
+          <div className="flex w-max gap-4 pb-4">
+            {barbershops.map((barbershop) => (
+              <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+            ))}
+          </div>
+
+          <ScrollBar orientation="horizontal" />
+        </ScrollArea>
       </div>
     </div>
   )
