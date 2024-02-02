a352e6daf2fadebda31cd8a2c3943642ed6893b0
Gabriel Martins <gabrielsilva3553@gmail.com>
2024-02-02 09:44:53 -0300

    style: add custom props to component

--- src/app/(home)/components/input-search.tsx
+++ src/app/(home)/components/input-search.tsx
@@ -1,10 +1,14 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Search } from 'lucide-react'
+import { ComponentProps } from 'react'
+import { twMerge } from 'tailwind-merge'
 
-export function InputSearch() {
+type InputSearchProps = ComponentProps<'div'>
+
+export function InputSearch({ className }: InputSearchProps) {
   return (
-    <div className="flex items-center gap-2">
+    <div className={twMerge('flex items-center gap-2', className)}>
       <Input placeholder="Busque por uma barbearia..." />
       <Button size="icon">
         <Search size={20} className="text-zinc-50" />
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,19 @@
 import Image from 'next/image'
 import { Button } from './ui/button'
 import { Menu } from 'lucide-react'
+import { ComponentProps } from 'react'
+import { twMerge } from 'tailwind-merge'
 
-export function Header() {
+type HeaderProps = ComponentProps<'header'>
+
+export function Header({ className }: HeaderProps) {
   return (
-    <header className="flex items-center justify-between border-b py-8 dark:border-zinc-900">
+    <header
+      className={twMerge(
+        'flex items-center justify-between border-b py-8 dark:border-zinc-900',
+        className,
+      )}
+    >
       <Image src="/logo.png" alt="logo Barber Blade" height={34} width={151} />
       <Button variant="ghost" size="icon">
         <Menu className="dark:text-zinc-50" />
